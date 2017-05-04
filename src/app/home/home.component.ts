a68ea9211d0c8adefcc61a55e5e2fcdcc1d4d7ef
Nathan Grove <ngrove@exegy.com>
2017-05-04 16:47:25 -0500

    added a user api call fetch

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '../services/http-client.service';
+
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  users: any;
+
+  constructor(
+    private http: HttpClient
+  ) { }
 
   ngOnInit() {
+
+    // make an api call. Treat the "HttpClient" service just as you would the standard Http service.
+    this.http.get('/api/now/table/sys_user?sysparm_fields=first_name,last_name,email,sys_created_on').subscribe( res => {
+
+      // let's take the response, parse the dates and store it in a users array 
+      this.users = res.json().result.map( user => {
+        user.sys_created_on = new Date(user.sys_created_on);
+        return user;
+
+      // a quick and dirty sort...
+      }).sort( (a,b) => `${a.first_name}${a.last_name}` < `${b.first_name}${b.last_name}` ? -1 : 1);
+
+    });
   }
 
 }
